49a5e1b63474ba6b086957e59018053091aa7c21
Anthony Ohiero Lifu <tonylifu@outlook.com>
2024-03-08 11:09:28 +0000

    refactors tf file adds arn

--- main.tf
+++ main.tf
@@ -1,3 +1,52 @@
 terraform {
-    
+    required_version = ">= 1.2"
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = ">= 5.39.1"
+      }
+    }
+}
+
+variable "aws_region" {
+  type = string
+  default = "eu-west-2"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+data "archive_file" "myzip" {
+  type = "zip"
+  source_file = "main.py"
+  output_path = "main.zip"
+}
+
+resource "aws_lambda_function" "mypython_lambda" {
+  filename = "main.zip"
+  function_name = "mypython_lambda_test"
+  role = aws_iam_role.mypython_lambda_role.arn
+  handler = "lambda_handler"
+  runtime = "python3.8"
+}
+
+resource "aws_iam_role" "mypython_lambda_role" {
+  name = "mypython_role"
+  
+  assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "sts:AssumeRole",
+            "Principal": {
+                "Service": "lambda.amazonaws.com"
+            }
+            "Effect": "Allow",
+            "Sid": ""
+        }
+    ]
+  }
+  EOF
 }
